b8d2bafe102b2010124ccedcbdb2f8d05c570dae
sahil1213-ux <sahilanand00027@gmail.com>
2024-09-18 14:27:44 +0530

    vite configured to make live project

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/SocialMediaWeb/",
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
